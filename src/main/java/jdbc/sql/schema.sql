a100955e7db37ad29824b7f8da05b546065039bc
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-04 20:13:28 +0300

    feat: add schema.sql including Product and Product_category table creation

---
+++ src/main/java/jdbc/sql/schema.sql
@@ -0,0 +1,14 @@
+
+-- create product table
+CREATE TABLE Product (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+price NUMERIC(10, 2) NOT NULL,
+creation_datetime TIMESTAMP NOT NULL
+);
+
+-- create product_category table
+CREATE TABLE Product_category
+(id SERIAL PRIMARY KEY,name VARCHAR(255) NOT NULL,
+product_id INT NOT NULL,FOREIGN KEY (product_id) REFERENCES Product(id)
+);
