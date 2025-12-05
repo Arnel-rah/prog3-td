e553a2aadb6b4350b49ca7e00457f8aa6ae1ed75
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-05 11:50:29 +0300

    feat: add domain models Category and Product with fields and basic structure

---
+++ src/main/java/jdbc/Category.java
@@ -0,0 +1,19 @@
+package jdbc;
+
+public class Category {
+    private final int id;
+    private final String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
