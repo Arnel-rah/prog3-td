1242fad8bea2491abc73c31a9e30d7e24aa653a9
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-06 10:10:00 +0300

    feat: implement getAllCategories() using Statement to fetch category list

--- src/main/java/jdbc/classe/DataRetrieve.java
+++ src/main/java/jdbc/classe/DataRetrieve.java
@@ -1,5 +1,29 @@
 package jdbc.classe;
 
-public class DataRetrieve {
+import jdbc.db.DBConnection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRetrieve {
+    public static List<Category> getAllCategories() throws SQLException {
+        List<Category> categories = new ArrayList<>();
+        try(Connection con = DBConnection.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT id, name FROM category")
+        ) {
+            while (rs.next()) {
+                Category c = new Category(
+                        rs.getInt("id"),
+                        rs.getString("name")
+                );
+                categories.add(c);
+            }
+            return categories;
+        }
+    }
 }
