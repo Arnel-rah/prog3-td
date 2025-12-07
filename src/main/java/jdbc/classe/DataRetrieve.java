bfe6ea0cd9a2c355beb6607642b3227ee9e3a539
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-07 20:20:02 +0300

    refactor: clean JDBC data retrieval logic and add full criteria filtering with pagination

--- src/main/java/jdbc/classe/DataRetrieve.java
+++ src/main/java/jdbc/classe/DataRetrieve.java
@@ -1,6 +1,6 @@
 package jdbc.classe;
 
-import java.sql.Timestamp;
+import java.security.Timestamp;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -48,49 +48,5 @@ public class DataRetrieve {
             throw new RuntimeException(e);
         }
     }
-
-    public static List<Product> getProductsByCriteria(
-            String productName, String categoryName,
-            Instant creationMin, Instant creationMax) {
-    List<Product> products = new ArrayList<>();
-
-        String sql = "SELECT p.* FROM product p " + "INNER JOIN category c ON p.category = c.id " + "WHERE 1 = 1";
-
-        if (productName != null && !productName.isEmpty()) {
-            sql += " AND p.name ILIKE '%" + productName.replace("'", "''") + "%'";
-        }
-
-        if (categoryName != null && !categoryName.isEmpty()) {
-            sql += " AND c.name ILIKE '%" + categoryName.replace("'", "''") + "%'";
-        }
-
-        if (creationMin != null) {
-            sql += " AND p.creationdatetime >= '" + Timestamp.from(creationMin) + "'";
-        }
-
-        if (creationMax != null) {
-            sql += " AND p.creationdatetime <= '" + Timestamp.from(creationMax) + "'";
-        }
-
-
-        try (Connection con = DBConnection.getConnection();
-         Statement statement = con.createStatement();
-         ResultSet rs = statement.executeQuery(sql.toString())) {
-        
-        while (rs.next()) {
-            products.add(new Product(
-                rs.getInt("id"),
-                rs.getString("name"),
-                rs.getTimestamp("creationdatetime").toInstant(),
-                (Category) rs.getObject("category")
-            ));
-        }
-        
-    } catch (SQLException e) {
-        throw new RuntimeException(e);
-    }
-    
-    return products;
-}
 
 }
